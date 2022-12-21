2fca660481fe5848b750fa446784c12f798ffcc7
0xBahamoot <lam@incognito.org>
2022-12-21 10:21:21 +0700

    update airdrop

--- constant.go
+++ constant.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	AirdropCoinValue       uint64 = 200
-	AirdropCoinShieldValue uint64 = 10000
+	AirdropCoinShieldValue uint64 = 300000000
 	PRVCoinPerTokenCoins          = 30
 	MaxTxOutput                   = 30
 )
